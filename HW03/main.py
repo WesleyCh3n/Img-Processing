32633f12d7c75041f36d53ae3604f3f1ae395743
WesleyCh3n <yoyochen0530@gmail.com>
2019-10-18 08:22:11 +0800

    update

--- HW03/main.py
+++ HW03/main.py
@@ -17,7 +17,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 	def openImg_click(self):
 		self.path = QFileDialog.getOpenFileName(self,"Open file","","Images(*.jpg)")
 		img = cv2.imread(self.path[0])
-		outImg = self.MatToQImage(img)		
+		print(img.dtype.name)
+		b,g,r = cv2.split(img)
+		print(b.dtype.name)
+		# b = np.split(img, 3, axis=2)[0].squeeze(axis=2).astype(np.uint8)
+		# g = np.split(img, 3, axis=2)[1].squeeze(axis=2).astype(np.uint8)
+		# r = np.split(img, 3, axis=2)[2].squeeze(axis=2).astype(np.uint8)
+		new_img = cv2.merge([r,g,b])
+		# new_img = np.stack((\
+		# 	np.zeros((img.shape[0],img.shape[1]),dtype=np.uint8),\
+		# 	np.zeros((img.shape[0],img.shape[1]),dtype=np.uint8),\
+		# 	r), axis=2)
+		print(new_img)
+		outImg = self.MatToQImage(new_img)		
 		self.label.setPixmap(outImg.scaled(self.label.width(),self.label.height(),Qt.KeepAspectRatio))
 
 	def MatToQImage(self, mat, swapped=True, qpixmap=True):
