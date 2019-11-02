a8de8aa6fd75cec90b6e571ece890ad4011159cd
WesleyCh3n <yoyochen0530@gmail.com>
2019-11-02 11:26:47 +0800

    Update main.py

--- HW04/main.py
+++ HW04/main.py
@@ -6,7 +6,7 @@ img = cv2.imread('C1HW04_IMG01_2019.jpg',0)
 print(img.shape)
 dft = cv2.dft(np.float32(img),flags = cv2.DFT_COMPLEX_OUTPUT)
 dft_shift = np.fft.fftshift(dft)
-
+print()
 magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
 
 plt.subplot(121),plt.imshow(img, cmap = 'gray')
