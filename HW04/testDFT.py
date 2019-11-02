f98ffdbf2733e5735245fd8be51c1b694d5c9c8c
WesleyCh3n <yoyochen0530@gmail.com>
2019-11-02 20:17:39 +0800

    change name

--- HW04/testDFT.py
+++ HW04/testDFT.py
@@ -3,11 +3,14 @@ import cv2
 import matplotlib.pyplot as plt
 
 img = cv2.imread('C1HW04_IMG01_2019.jpg',0)
-print(img.shape)
-dft = cv2.dft(np.float32(img),flags = cv2.DFT_COMPLEX_OUTPUT)
+print(f"This is input shape {img.shape}")
+dft = cv2.dft(np.float32(img), flags = cv2.DFT_COMPLEX_OUTPUT)
+print(f"This is dft shape {dft.shape}")
+print(dft)
 dft_shift = np.fft.fftshift(dft)
-print()
+print(f"This is dft_shift shape {dft_shift.shape}")
 magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
+print(f"This is magnitude_spectrum shape {magnitude_spectrum.shape}")
 
 plt.subplot(121),plt.imshow(img, cmap = 'gray')
 plt.title('Input Image'), plt.xticks([]), plt.yticks([])
