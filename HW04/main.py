3e453a3c814e5287b10d292fc35df85cf5dcfea9
WesleyCh3n <yoyochen0530@gmail.com>
2019-11-04 14:27:37 +0800

    update

--- HW04/main.py
+++ HW04/main.py
@@ -5,6 +5,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import qdarkstyle
 import cv2
+import math
 import numpy as np
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -16,6 +17,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 		self.dftBn.clicked.connect(self.fftBn_clicked)
 		self.idLowBn.clicked.connect(self.idLowBn_clicked)
 		self.bLowBn.clicked.connect(self.bLowBn_clicked)
+		self.GauLowBn.clicked.connect(self.GauLowBn_clicked)
+		self.idHighBn.clicked.connect(self.idHighBn_clicked)
+		self.bHighBn.clicked.connect(self.bHighBn_clicked)
+		self.GauHighBn.clicked.connect(self.GauHighBn_clicked)
+		self.homoBn.clicked.connect(self.homoBn_clicked)
 		# inImg = cv2.imread('C1HW04_IMG01_2019.jpg', 0)
 
 	def openImg_clicked(self):
@@ -43,45 +49,140 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 		outImg1 = self.MatToQImage(ang)
 		outImg = self.MatToQImage(magnitude_spectrum)
 		self.inputLb.setPixmap(outImg.scaled(self.inputLb.width(),self.inputLb.height(),Qt.KeepAspectRatio))
-		self.imgShowLb.setPixmap(outImg1.scaled(self.imgShowLb.width(),self.imgShowLb.height(),Qt.KeepAspectRatio))
+		self.outputLb.setPixmap(outImg1.scaled(self.outputLb.width(),self.outputLb.height(),Qt.KeepAspectRatio))
 
 	def idLowBn_clicked(self):
 		inImg = cv2.imread(self.path[0], 0)
 		dft = cv2.dft(np.float32(inImg), flags = cv2.DFT_COMPLEX_OUTPUT)
 		dft_shift = np.fft.fftshift(dft)
 		rows, cols = inImg.shape
-		crow, ccol = int(rows/2) , int(cols/2)
 		mask = np.zeros((rows, cols, 2), np.uint8)
-		mask[crow-30:crow+30, ccol-30:ccol+30] = 1
+		for u in range(rows):
+			for v in range(cols):
+				radius = (((u - int(rows/2))/2)**2+((v - int(cols/2))/2)**2)**(1/2)
+				if radius <= 10:
+					mask[u, v, 0] = 1
+					mask[u, v, 1] = 1
+				else:
+					mask[u, v, 0] = 0
+					mask[u, v, 1] = 0
 		filterOut = dft_shift*mask
 		f_ishift = np.fft.ifftshift(filterOut)
 		back = cv2.idft(f_ishift,flags=cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT)
-		back[back <= 0] = 0
-		back[back >= 255] = 255
 		outImg = self.MatToQImage(back)
-		self.imgShowLb.setPixmap(outImg.scaled(self.imgShowLb.width(),self.imgShowLb.height(),Qt.KeepAspectRatio))
+		self.outputLb.setPixmap(outImg.scaled(self.outputLb.width(),self.outputLb.height(),Qt.KeepAspectRatio))
 
 	def bLowBn_clicked(self):
 		inImg = cv2.imread(self.path[0], 0)
 		dft = cv2.dft(np.float32(inImg), flags = cv2.DFT_COMPLEX_OUTPUT)
 		dft_shift = np.fft.fftshift(dft)
 		rows, cols = inImg.shape
-		crow, ccol = int(rows/2) , int(cols/2)
-		mask = np.zeros((rows, cols), np.float32)
+		mask = np.zeros((rows, cols, 2), np.float32)
 		for u in range(rows):
 			for v in range(cols):
-				radius = (((u - crow)/2)**2+((v - ccol)/2)**2)**(1/2)
-				mask[u, v] = 1.0/((1+(radius/40))**4)
-		mask = np.repeat(mask[:, :, np.newaxis], 2, axis=2)
+				radius = (((u - int(rows/2))/2)**2+((v - int(cols/2))/2)**2)**(1/2)
+				mask[u, v, 0] = 1.0/((1+(radius/40))**4)
+				mask[u, v, 1] = 1.0/((1+(radius/40))**4)
 		filterOut = dft_shift*mask
 		f_ishift = np.fft.ifftshift(filterOut)
 		back = cv2.idft(f_ishift,flags=cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT)
 		outImg = self.MatToQImage(back)
-		self.imgShowLb.setPixmap(outImg.scaled(self.imgShowLb.width(),self.imgShowLb.height(),Qt.KeepAspectRatio))
+		self.outputLb.setPixmap(outImg.scaled(self.outputLb.width(),self.outputLb.height(),Qt.KeepAspectRatio))
 
-	# def 
+	def GauLowBn_clicked(self):
+		inImg = cv2.imread(self.path[0], 0)
+		dft = cv2.dft(np.float32(inImg), flags = cv2.DFT_COMPLEX_OUTPUT)
+		dft_shift = np.fft.fftshift(dft)
+		rows, cols = inImg.shape
+		mask = np.zeros((rows, cols, 2), np.float32)
+		for u in range(rows):
+			for v in range(cols):
+				radius = (((u - int(rows/2))/2)**2+((v - int(cols/2))/2)**2)**(1/2)
+				mask[u, v, 0] = math.exp((-1*radius**2)/(2*(1**2)))
+				mask[u, v, 1] = math.exp((-1*radius**2)/(2*(1**2)))
+		filterOut = dft_shift*mask
+		f_ishift = np.fft.ifftshift(filterOut)
+		back = cv2.idft(f_ishift,flags=cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT)
+		outImg = self.MatToQImage(back)
+		self.outputLb.setPixmap(outImg.scaled(self.outputLb.width(),self.outputLb.height(),Qt.KeepAspectRatio))
 
+	def idHighBn_clicked(self):
+		inImg = cv2.imread(self.path[0], 0)
+		dft = cv2.dft(np.float32(inImg), flags = cv2.DFT_COMPLEX_OUTPUT)
+		dft_shift = np.fft.fftshift(dft)
+		rows, cols = inImg.shape
+		mask = np.zeros((rows, cols, 2), np.uint8)
+		for u in range(rows):
+			for v in range(cols):
+				radius = (((u - int(rows/2))/2)**2+((v - int(cols/2))/2)**2)**(1/2)
+				if radius > 10:
+					mask[u, v, 0] = 1
+					mask[u, v, 1] = 1
+				else:
+					mask[u, v, 0] = 0
+					mask[u, v, 1] = 0
+		filterOut = dft_shift*mask
+		f_ishift = np.fft.ifftshift(filterOut)
+		back = cv2.idft(f_ishift,flags=cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT)
+		outImg = self.MatToQImage(back)
+		self.outputLb.setPixmap(outImg.scaled(self.outputLb.width(),self.outputLb.height(),Qt.KeepAspectRatio))
+
+	def bHighBn_clicked(self):
+		inImg = cv2.imread(self.path[0], 0)
+		dft = cv2.dft(np.float32(inImg), flags = cv2.DFT_COMPLEX_OUTPUT)
+		dft_shift = np.fft.fftshift(dft)
+		rows, cols = inImg.shape
+		mask = np.zeros((rows, cols, 2), np.float32)
+		for u in range(rows):
+			for v in range(cols):
+				radius = (((u - int(rows/2))/2)**2+((v - int(cols/2))/2)**2)**(1/2)
+				mask[u, v, 0] = 1.0 - (1.0/((1.0+(radius/40.0))**2))
+				mask[u, v, 1] = 1.0 - (1.0/((1.0+(radius/40.0))**2))
+		filterOut = dft_shift*mask
+		f_ishift = np.fft.ifftshift(filterOut)
+		back = cv2.idft(f_ishift,flags=cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT)
+		outImg = self.MatToQImage(back)
+		self.outputLb.setPixmap(outImg.scaled(self.outputLb.width(),self.outputLb.height(),Qt.KeepAspectRatio))
+
+	def GauHighBn_clicked(self):
+		inImg = cv2.imread(self.path[0], 0)
+		dft = cv2.dft(np.float32(inImg), flags = cv2.DFT_COMPLEX_OUTPUT)
+		dft_shift = np.fft.fftshift(dft)
+		rows, cols = inImg.shape
+		mask = np.zeros((rows, cols, 2), np.float32)
+		for u in range(rows):
+			for v in range(cols):
+				radius = (((u - int(rows/2))/2)**2+((v - int(cols/2))/2)**2)**(1/2)
+				mask[u, v, 0] = 1.0 - math.exp((-1*radius**2)/(2*(1**2)))
+				mask[u, v, 1] = 1.0 - math.exp((-1*radius**2)/(2*(1**2)))
+		filterOut = dft_shift*mask
+		f_ishift = np.fft.ifftshift(filterOut)
+		back = cv2.idft(f_ishift,flags=cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT)
+		outImg = self.MatToQImage(back)
+		self.outputLb.setPixmap(outImg.scaled(self.outputLb.width(),self.outputLb.height(),Qt.KeepAspectRatio))
+
+	def homoBn_clicked(self):
+		inImg = cv2.imread(self.path[0], 0)
+		dft = cv2.dft(np.float32(inImg), flags = cv2.DFT_COMPLEX_OUTPUT)
+		dft_shift = np.fft.fftshift(dft)
+		rows, cols = inImg.shape
+		mask = np.zeros((rows, cols, 2), np.float32)
+		gammaH = 3.0
+		gammaL = 0.4
+		for u in range(rows):
+			for v in range(cols):
+				radius = (((u - int(rows/2))/2)**2+((v - int(cols/2))/2)**2)**(1/2)
+				mask[u, v, 0] = (gammaH - gammaL)*(1.0 - math.exp(-2*(radius**2)/(20.0)**2))+gammaL
+				mask[u, v, 1] = (gammaH - gammaL)*(1.0 - math.exp(-2*(radius**2)/(20.0)**2))+gammaL
+		filterOut = dft_shift*mask
+		f_ishift = np.fft.ifftshift(filterOut)
+		back = cv2.idft(f_ishift,flags=cv2.DFT_SCALE | cv2.DFT_REAL_OUTPUT)
+		outImg = self.MatToQImage(back)
+		self.outputLb.setPixmap(outImg.scaled(self.outputLb.width(),self.outputLb.height(),Qt.KeepAspectRatio))
+		
 	def MatToQImage(self, mat, swapped=True, qpixmap=True):
+		mat[mat >= 255] = 255
+		mat[mat <= 0] = 0
 		if mat.ndim == 2:
 			mat = np.repeat(mat[:, :, np.newaxis], 3, axis=2)
 		if mat.dtype != np.uint8:
