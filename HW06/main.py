372cb8d394bbf3f7cd4f428d3d0779e070b61794
WesleyCh3n <yoyochen0530@gmail.com>
2019-12-02 16:21:47 +0800

    update wavy

--- HW06/main.py
+++ HW06/main.py
@@ -12,20 +12,14 @@ transImg = np.zeros((rows, cols, 3), np.uint8)
 #         transImg[int(3*y/4+x*y/(rows*cols))][int(x+y/4-x*y/(2*rows))] = img[y][x]
 
 ### Wavy Transformation
-# for y in range(rows):
-#     for x in range(cols):
-#         x_mod = x+60*np.sin(np.pi/100*y)
-#         if x_mod >= rows: x_mod = rows-1
-#         elif x_mod < 0: x_mod = 0
-#         transImg[int(x_mod)][y] = img[x][y]
-# 
-# transImg2 = np.zeros((rows, cols, 3), np.uint8)
-# for y in range(rows):
-#     for x in range(cols):
-#         y_mod = y+60*np.sin(np.pi/100*x)
-#         if y_mod >= rows: y_mod =  cols-1
-#         elif y_mod < 0: y_mod = 0
-#         transImg2[x][int(y_mod)] = transImg[x][y]
+for y in range(rows):
+    for x in range(cols):
+        x_offset = x + int(50.0 * np.sin(0.5 * np.pi * y / 150)) 
+        y_offset = y + int(50.0 * np.cos(0.5 * np.pi * x / 150))
+        if x_offset > 0 and y_offset > 0:
+            if y_offset<rows and x_offset < cols:
+                transImg[y][x] = img[(y_offset)%rows][(x_offset)%cols]
+
 # plt.subplot(1,2,1), plt.imshow(transImg[:,:,::-1])
 # plt.subplot(1,2,2), plt.imshow(transImg2[:,:,::-1])
 
@@ -39,7 +33,7 @@ transImg = np.zeros((rows, cols, 3), np.uint8)
 #        x_mod = x_ratio * np.sqrt(1 - y_ratio**2/2) * xR + xR
 #        y_mod = y_ratio * np.sqrt(1 - x_ratio**2/2) * yR + yR
 #        transImg[int(x_mod)][int(y_mod)] = img[x][y]
-#plt.imshow(transImg[:,:,::-1])
+plt.imshow(transImg[:,:,::-1])
 
 # plt.savefig('saveImg.png')
 plt.show()
