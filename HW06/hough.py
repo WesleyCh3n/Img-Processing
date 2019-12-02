8ce76347cb6ca7dca3535d2f83fa20e147128244
WesleyCh3n <yoyochen0530@gmail.com>
2019-12-02 17:42:25 +0800

    update

--- HW06/hough.py
+++ HW06/hough.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 img = cv2.imread('rects.bmp', 0)
-print(img.shape)
 edgesImg = cv2.Canny(img ,50,150,apertureSize = 3) 
 lines = cv2.HoughLinesP(edgesImg, 1,np.pi/360, 50,  None , 50, 10)
 numbers, dummy, points = lines.shape
 for x1, y1, x2, y2 in lines.reshape((numbers,points)):
-    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),10)
-print(lines.shape)
+    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),5)
+    print(x1, y1, x2, y2)
+    cv2.putText(img, f"{np.sqrt((x1-x2)**2+(y1-y2)**2):.2f}", (int(round((x2+x1)/2)), int(round((y1+y2)/2))), cv2.FONT_HERSHEY_DUPLEX, 0.7, (255,255,255), 1, cv2.LINE_AA)
 plt.imshow(img)
 plt.show()
