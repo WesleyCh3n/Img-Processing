d204a50eca277bd4b8ff783820406530fd56c549
WesleyCh3n <yoyochen0530@gmail.com>
2019-12-03 22:50:27 +0800

    update

--- HW06/main.py
+++ HW06/main.py
@@ -6,6 +6,8 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from myui import *
 from wrapthread import wrap
+import matplotlib.pyplot as plt
+import pywt
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent = None):
@@ -20,7 +22,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.cirBn.clicked.connect(self.cirBn_clicked)
         self.houBn.clicked.connect(self.houBn_clicked)
         self.openmultiBn.clicked.connect(self.openmultiBn_clicked)
-        self.dwtBn.clicked.connect(self.dwtBn_clicked)
+        # self.dwtBn.clicked.connect(self.dwtBn_clicked)
 
         self.htsB.setRange(1, 100)
         self.htsB.setValue(51)
@@ -77,11 +79,48 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.showImg(outImg)
 
     def openmultiBn_clicked(self):
-        path = QFileDialog.getOpenFileNames(self,"Open file","","Images(*.JPG *.jpg *.bmp)")
-        print(path)
+        path, fileExt = QFileDialog.getOpenFileNames(self,"Open file","","Images(*.JPG *.jpg *.bmp)")
+        imgList = []
+        coeffList = []
+        caList = []
+        chList = []
+        cvList = []
+        cdList = []
+        for i in range(len(path)):
+            imgList.append(cv2.imread(path[i], 0))
+        x, y = imgList[0].shape
+        for i in range(len(path)):
+            imgList[i] = cv2.resize(imgList[i], (y, x))
+            coeffList.append(pywt.dwt2(imgList[i], 'haar'))
+            caList.append(coeffList[i][0])
+            chList.append(coeffList[i][1][0])
+            cvList.append(coeffList[i][1][1])
+            cdList.append(coeffList[i][1][2])
+        ca = sum(caList) / len(caList)
+        if len(chList) == 2:
+            ch = np.maximum(chList[0], chList[1])
+        if len(chList) == 3:
+            ch = np.maximum(chList[0], chList[1], chList[2])
+        if len(cvList) == 2:
+            cv = np.maximum(cvList[0], cvList[1])
+        if len(cvList) == 3:
+            cv = np.maximum(cvList[0], cvList[1], cvList[2])
+        if len(cdList) == 2:
+            cd = np.maximum(cdList[0], cdList[1])
+        if len(cdList) == 3:
+            cd = np.maximum(cdList[0], cdList[1], cdList[2])
+        coeff = ca, (ch, cv, cd)
+        resultImg = pywt.idwt2(coeff, 'haar').astype(np.uint8)
+        resultImg = np.repeat(resultImg[:, :, np.newaxis], 3, axis=2)
+        for i in range(len(imgList)):
+            plt.subplot(2, len(imgList), i+1), plt.imshow(imgList[i], cmap='gray')
+            plt.title(f'Input image {i+1}', fontsize = 10)
+        plt.subplot(2, 1, 2), plt.imshow(resultImg)
+        plt.title(f'Result', fontsize = 10)
+        plt.show()
 
-    def dwtBn_clicked(self):
-        return 0
+    # def dwtBn_clicked(self):
+        # return 0
 
         
     def MatToQImage(self, mat, swapped=True, qpixmap=True):
