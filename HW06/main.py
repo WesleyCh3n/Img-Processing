5c4f129ba4781cfcc620b90a9e6512e5bb6ae0ad
WesleyCh3n <yoyochen0530@gmail.com>
2019-12-03 14:25:48 +0800

    update except DWT

--- HW06/main.py
+++ HW06/main.py
@@ -16,6 +16,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.actionOpen_Image.triggered.connect(self.actionOpen_file_triggered)
         self.trapBn.clicked.connect(self.trapBn_clicked)        
+        self.wavyBn.clicked.connect(self.wavyBn_clicked)
+        self.cirBn.clicked.connect(self.cirBn_clicked)
+        self.houBn.clicked.connect(self.houBn_clicked)
+
+        self.htsB.setRange(1, 100)
+        self.htsB.setValue(51)
+        self.hmsB.setRange(1, 100)
+        self.hmsB.setValue(50)
+        self.sB.setRange(1, 100)
+        self.sB.setValue(50)
+        self.dsB.setRange(0.01, 100)
+        self.dsB.setValue(2)
+        self.dsB.setSingleStep(0.1)
 
         self.wrapObj = wrap()
         self.wrapObj.progress.connect(self.progressBar.setValue)
@@ -24,14 +37,43 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def actionOpen_file_triggered(self):
         self.path = QFileDialog.getOpenFileName(self,"Open file","","Images(*.jpg *.bmp)")
         self.inImg = cv2.imread(self.path[0])
-        #self.threadObj.inImg = self.inImg
+        self.wrapObj.inImg = self.inImg
         self.showImg(self.inImg, True)
 
     def trapBn_clicked(self):
-        self.wrapObj.inImg = self.inImg
+        if self.path == False: return QMessageBox.warning(self, "WARNING", "The input image is empty")
         self.wrapObj.trapFlag = True
         self.wrapObj.start()
 
+    def wavyBn_clicked(self):
+        if self.path == False: return QMessageBox.warning(self, "WARNING", "The input image is empty")
+        self.wrapObj.amp = self.sB.value()
+        self.wrapObj.freq = self.dsB.value()
+        self.wrapObj.wavyFlag = True
+        self.wrapObj.start()
+
+    def cirBn_clicked(self):
+        if self.path == False: return QMessageBox.warning(self, "WARNING", "The input image is empty")
+        self.wrapObj.cirFlag = True
+        self.wrapObj.start()
+
+    def houBn_clicked(self):
+        if self.path == False: return QMessageBox.warning(self, "WARNING", "The input image is empty")
+        img = cv2.cvtColor(self.inImg, cv2.COLOR_BGR2GRAY)
+        outImg = np.copy(self.inImg)
+        edgesImg = cv2.Canny(img ,50,150,apertureSize = 3) 
+        lines = cv2.HoughLinesP(edgesImg, 1, np.pi/360, self.htsB.value(), None, self.hmsB.value(), 10)
+        numbers, dummy, points = lines.shape
+        len_sum = 0
+        for x1, y1, x2, y2 in lines.reshape((numbers,points)):
+            cv2.line(outImg, (x1,y1), (x2,y2), (0,255,0), 5)
+            len_sum += np.sqrt((x1-x2)**2+(y1-y2)**2)
+        for x1, y1, x2, y2 in lines.reshape((numbers,points)):
+            cv2.putText(outImg, f"{np.sqrt((x1-x2)**2+(y1-y2)**2):.2f} pixels", (int(round((x2+x1)/2)), int(round((y1+y2)/2))), cv2.FONT_HERSHEY_DUPLEX, 0.6, (0, 0, 0), 1, cv2.LINE_AA)
+        self.textBrowser.append(f"Total pixel length is {len_sum:.2f}")
+        self.textBrowser.append(f"There are {numbers} lines\n"+"="*16)
+        self.showImg(outImg)
+        
     def MatToQImage(self, mat, swapped=True, qpixmap=True):
         mat[mat >= 255] = 255
         mat[mat <= 0] = 0
--- HW06/wrapthread.py
+++ HW06/wrapthread.py
@@ -9,6 +9,10 @@ class wrap(QThread):
         super().__init__(parent)
         self.inImg = False
         self.trapFlag = False
+        self.wavyFlag = False
+        self.cirFlag = False
+        self.amp = 50
+        self.freq = 2
 
     def trap(self):
         rows, cols, dims = self.inImg.shape
@@ -19,10 +23,44 @@ class wrap(QThread):
             self.progress.emit(y*101/rows)
         self.processImg.emit(transImg)
     
-    def resetFlag(self):
-        self.trapFlag == False
+    def wavy(self):
+        rows, cols, dims = self.inImg.shape
+        transImg = np.zeros((rows, cols, 3), np.uint8)
+        for y in range(rows):
+            for x in range(cols):
+                x_offset = x + int(self.amp * np.sin(self.freq * np.pi * y / 150)) 
+                y_offset = y + int(self.amp * np.cos(self.freq * np.pi * x / 150))
+                if x_offset > 0 and y_offset > 0:
+                    if y_offset<rows and x_offset < cols:
+                        transImg[y][x] = self.inImg[(y_offset)%rows][(x_offset)%cols]
+            self.progress.emit(y*101/rows)
+        self.processImg.emit(transImg)
 
+    def cir(self):
+        rows, cols, dims = self.inImg.shape
+        transImg = np.zeros((rows, cols, 3), np.uint8)
+        xR = rows/2.0
+        yR = cols/2.0
+        for y in range(cols):
+            for x in range(rows):
+                x_ratio = (x - xR)/xR
+                y_ratio = (y - yR)/yR
+                x_mod = x_ratio * np.sqrt(1 - y_ratio**2/2) * xR + xR
+                y_mod = y_ratio * np.sqrt(1 - x_ratio**2/2) * yR + yR
+                transImg[int(x_mod)][int(y_mod)] = self.inImg[x][y]
+            self.progress.emit(y*101/rows)
+        self.processImg.emit(transImg)
+        
+    def resetFlag(self):
+        self.trapFlag = False
+        self.wavyFlag = False
+        self.cirFlag = False
+    
     def run(self):
         if self.trapFlag == True:
             self.trap()
+        if self.wavyFlag == True:
+            self.wavy()
+        if self.cirFlag == True:
+            self.cir()
         self.resetFlag()
