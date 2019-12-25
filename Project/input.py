614932a2b849b843ab99f10c4e738b28048b41a1
WesleyCh3n <yoyochen0530@gmail.com>
2019-12-25 16:39:40 +0800

    update

--- Project/input.py
+++ Project/input.py
@@ -1,6 +1,9 @@
 import pytesseract
 import cv2
+import matplotlib.pyplot as plt
 
 img = cv2.imread('./test.jpg')
 text = pytesseract.image_to_string(img, lang='eng')
 print(text)
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.show()
