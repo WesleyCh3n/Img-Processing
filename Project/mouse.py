e19fb21763eec5d3a32b7bcdb26d0479ed2dab22
WesleyCh3n <yoyochen0530@gmail.com>
2019-12-28 11:27:50 +0800

    update

---
+++ Project/mouse.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import cv2
+
+img = cv2.imread('./img.jpg')
+ax = plt.gca()
+fig = plt.gcf()
+implot = ax.imshow(img)
+
+def onclick(event):
+    if event.xdata != None and event.ydata != None:
+        print(event.xdata, event.ydata)
+        plt.plot(event.xdata, event.ydata, '.')
+        fig.canvas.draw()
+
+cid = fig.canvas.mpl_connect('button_press_event', onclick)
+# plt.draw()
+
+plt.show()
+
