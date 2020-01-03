f5bd1c0ef0ededc626b299df60189eb7e6781dfe
WesleyCh3n <yoyochen0530@gmail.com>
2020-01-03 16:47:11 +0800

    update

--- Project/input.py
+++ Project/input.py
@@ -35,15 +35,30 @@ img = cv2.imread('./test.jpg')
 # f.close()
 
 '''Put text on new image'''
+# df = pd.read_csv('./word.csv')
+# df['text'].replace('', np.nan, inplace=True)
+# df.dropna(subset=['text'], inplace=True)
+
+print(img.shape)
+# newImg = np.ones(img.shape, np.uint8) * 255
+# for i in range(len(df['level'])):
+#     (x, y, w, h) = (df['left'].iloc[i], df['top'].iloc[i], df['width'].iloc[i], df['height'].iloc[i])
+#     cv2.putText(newImg, str(df['text'].iloc[i]), (x, y), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0,0,0), 1, cv2.LINE_AA)
+
+# plt.imshow(cv2.cvtColor(newImg, cv2.COLOR_BGR2RGB))
+# plt.show()
+
+'''PDF output'''
 df = pd.read_csv('./word.csv')
 df['text'].replace('', np.nan, inplace=True)
 df.dropna(subset=['text'], inplace=True)
+(x, y, w, h) = (df['left'].iloc[0], df['top'].iloc[0], df['width'].iloc[0], df['height'].iloc[0])
+print(x, y, w, h)
 
-print(img.shape)
-newImg = np.ones(img.shape, np.uint8) * 255
-for i in range(len(df['level'])):
-    (x, y, w, h) = (df['left'].iloc[i], df['top'].iloc[i], df['width'].iloc[i], df['height'].iloc[i])
-    cv2.putText(newImg, str(df['text'].iloc[i]), (x, y), cv2.FONT_HERSHEY_DUPLEX, 0.7, (0,0,0), 1, cv2.LINE_AA)
-
-plt.imshow(cv2.cvtColor(newImg, cv2.COLOR_BGR2RGB))
-plt.show()
+from fpdf import FPDF
+pdf = FPDF()
+pdf.set_font('Arial', '', 12)
+pdf.add_page()
+pdf.cell(x*(25.4/96)*(2480/640), y*(25.4/96)*(2480/640), df['text'].iloc[0])
+print(x*(25.4/96)*(2480/640), y*(25.4/96)*(2480/640))
+pdf.output('tes.pdf', 'F')
