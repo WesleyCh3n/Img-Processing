b13c34720b5b571c871b2a8ae87fe3694b8ed7c6
WesleyCh3n <yoyochen0530@gmail.com>
2020-01-04 00:10:04 +0800

    update

--- Project/input.py
+++ Project/input.py
@@ -52,8 +52,6 @@ print(img.shape)
 df = pd.read_csv('./word.csv')
 df['text'].replace('', np.nan, inplace=True)
 df.dropna(subset=['text'], inplace=True)
-# (x, y, w, h) = (df['left'].iloc[0], df['top'].iloc[0], df['width'].iloc[0], df['height'].iloc[0])
-# print(x, y, w, h)
 
 from fpdf import FPDF
 pdf = FPDF('P','pt','A4')
@@ -62,5 +60,5 @@ pdf.set_font('Arial', '', 12)
 for i in range(len(df['level'])):
     (x, y, w, h) = (df['left'].iloc[i], df['top'].iloc[i], df['width'].iloc[i], df['height'].iloc[i])
     pdf.set_xy(x, y)
-    pdf.cell(w, h, df['text'].iloc[i], ln=2)
+    pdf.cell(w, h, df['text'].iloc[i])
 pdf.output('tes.pdf')
